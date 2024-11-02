aba4583415e6d600368e33f42a366e64fc0807f9
TheTrueProblematic <mwmttl@gmail.com>
2024-11-02 08:21:39 -0600

    A basic stopwatch that starts, stops, and resets.

--- Stopwatch/Stopwatch.py
+++ Stopwatch/Stopwatch.py
@@ -1,50 +1,70 @@
 
-# Howdy partners! Welcome to this super fun stopwatch timer code. Yeehaw!
+# Hello there, coding preneur! Welcome to our lovely Stopwatch script!
+# This handy-dandy program is going to mimic a basic stopwatch.
+# It is capable of starting, stopping, and resetting (ain't that cool!).
+# It's a little digital buddy right in your terminal.
+# So sit tight, grab your popcorn and let's dive right in!
 
 import time
 
 class Stopwatch:
     def __init__(self):
-        # Ain't nothin' more important than time, right?
-        self.start_time = None
-        self.stop_time = None
+        self.start_time = 0
+        self.stop_time = 0
+        self.running = False
 
+    # Kickoff our stopwatch! Ain't that fun!
     def start(self):
-        # Get this rodeo started! Don't you worry if it's running already.
-        if self.start_time is None:
+        if not self.running:
             self.start_time = time.time()
-            self.stop_time = None
+            self.running = True
         else:
-            print("Hey partner, the stopwatch is running already. No need to start it again.") 
+            print("Hey there, Stopwatch is already running!")
 
+    # Like all good things, our stopwatch also needs to pause sometimes.
     def stop(self):
-        # Enough horsing around. It's time to stop.
-        if self.start_time is not None:
+        if self.running:
             self.stop_time = time.time()
-            self.start_time = None
+            self.running = False
         else:
-            print("Hold your horses! You can't stop what ain't started.")
+            print("Oops! Stopwatch is not running.")
 
+    # Time to get to the point, let's calculate lapsed time.
     def elapsed_time(self):
-        # Y'all wanna know how much time you've been spending? Here you go!
-        if self.stop_time is None:
+        if self.running:
             return time.time() - self.start_time
         else:
             return self.stop_time - self.start_time
 
+    # Goodbye past, hello future!
     def reset(self):
-        # Fresh start! It's like sunrise on the prairie.
-        self.start_time = None
-        self.stop_time = None
+        self.start_time = self.stop_time = 0
+        self.running = False
 
+
+# Here comes our main function! It will orchestrate our Stopwatch's performance.
+def main():
+    stopwatch = Stopwatch()
+    while True:
+        action = input("Enter 'S' to start, 'T' to stop, 'E' to show elapsed time, 'R' to reset, 'Q' to quit: ").upper()
+        if action == "S":
+            stopwatch.start()
+        elif action == "T":
+            stopwatch.stop()
+        elif action == "E":
+            print(f"Elapsed time: {stopwatch.elapsed_time()} seconds.")
+        elif action == "R":
+            stopwatch.reset()
+        elif action == "Q":
+            break
+        else:
+            print("Whoopsie daisy! Couldn't recognize your command. Kindly punch in again.")
+
+# Invoke the main function in python's style!
 if __name__ == "__main__":
-    # Time to put this horse in the race!
-    sw = Stopwatch()
-    sw.start()
-    time.sleep(1)  # Wait a jiffy, let's let the stopwatch tick for a sec.
-    print("Elapsed time: ", sw.elapsed_time())
-    sw.stop()
-    print("Time after stop: ", sw.elapsed_time())
-    sw.reset()
-    print("Time after reset: ", sw.elapsed_time())
+    main()
+
+# Woah, you've made it till the end. So proud of you! Wasn't it a thrilling ride?
+# Now, you can time yourself while you're coding away to glory!!! How cool is that!
+# Keep rocking and keep coding! Until next time, bye!
 
