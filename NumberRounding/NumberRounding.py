96e1757e50846c28f04dba9cf7753e62bf7e17f8
TheTrueProblematic <mwmttl@gmail.com>
2024-12-09 17:23:19 -0700

    Round numbers to the nearest whole number.

---
+++ NumberRounding/NumberRounding.py
@@ -0,0 +1,25 @@
+# Well, let's dive in and have some fun with Python, shall we?
+# Our mission today: Round numbers to the nearest whole number!
+# Exciting, isn't it?
+
+# And remember, no GUI, no dependencies, no Internet access, just pure Python.
+# So, let's get this party started!
+
+def round_to_nearest_whole(num):
+    # Python has a built-in function, round(), that we're going to use.
+    # It takes a number and rounds it to the nearest whole number. As simple as that!
+    # And we make it even simpler by putting it in a function!
+    return round(num)
+    
+if __name__ == "__main__":
+    # Uh-oh! There's a float number! It's all fine and stuff, but hey, we're in a mood for whole numbers today.
+    # Can we turn it into a whole number? Should we call Avengers? Superman? No, we have our very own function!
+    float_number = 12.7
+    rounded_number = round_to_nearest_whale(float_number)
+    
+    # Let's print out the result and see the magic happen!
+    print(f"The round off value of {float_number} is: {rounded_number}")
+
+# BOOM! Python magic in action.
+# Numbers rounded up all nice and neat, just the way we like it.
+# Another mission accomplished in the fabulous world of Python!
