9947f67b208b8d2d955561c2ef383ad748876cdb
TheTrueProblematic <mwmttl@gmail.com>
2025-01-05 11:53:07 -0700

    Convert an octal number to its decimal equivalent.

---
+++ OctalToDecimal/OctalToDecimal.py
@@ -0,0 +1,23 @@
+
+# Hello, my friend! Welcome to the OctalToDecimal python magic converter. 🎩🌟
+# This program is going to show you how easy-peasy-lemon-squeezy it is to change from octal to decimal number! 🐙➡️🔟
+
+# Ain't no rest for the wicked - code doesn't grow on trees, 
+# so let's not hesitate and dive right into this cool decoding adventure. 🚀
+
+# The first step in our journey is to grab that mysterious octal number from the user.
+# We're going to do this by using Python's built-in input function. 🗣️👂
+octal_number = input("Hello there, beautiful! Could you please type in your octal number? ")
+
+# Cool! Now that we've got the octal number, let's make some magic happen. 
+# We're going to use Python's int function. This wonderful little function
+# is going to take our octal number and treat it as base 8 (which is what octals are).
+# So, here is our magic spell: 🧙‍♀️
+decimal_number = int(octal_number, 8)
+
+# THAT'S IT! Yes, my coding comrade, it really is that simple. Python is doing all the hard work for us.
+# All we have to do now is print it for our user.   🎉
+print("Here you go, hot stuff! Your decimal number is: ", decimal_number)
+
+# Wasn't that a hoot? I can't wait for our next coding adventure together. Until then, 
+# remember: code is just magic waiting to happen! 🧙‍♂️🌟
