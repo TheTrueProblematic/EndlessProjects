5f4435737468f5cc3025cffe2e25ccdc1ccaeff8
TheTrueProblematic <mwmttl@gmail.com>
2025-04-25 17:39:12 -0600

    Implement a basic deque (double-ended queue) data structure.

--- DequeImplementation/DequeImplementation.py
+++ DequeImplementation/DequeImplementation.py
@@ -1,53 +1,61 @@
 
-# Hey there, fellow coder! Get ready to dive into some cool Python magic.
-# We're about to implement our very own Deque, a.k.a Double-Ended Queue!
-
-# Let's get started!
+#-----------------------------------------
+# Hey there traveller of code! 
+# Here we are on an exciting adventure, where we are exploring one of the coolest data structures - The Deque!
+# Deques are just like queues, but’s more chill! You can insert and remove elements from both ends, hence the name.
+# You can essentially look at it as a line in your favorite fast food place but where people can join and leave from both ends! Neat, isn’t it?
+# This Deque is going to be a basic one, but it would work perfectly. And remember, Great things have small beginnings!
+# Buckle up, we are diving into code!
+#-----------------------------------------
 
 class Deque:
-    
-    # We'll start by initializing our Deque. At this point, it's just an empty list.
+    #-----------------------------------------
+    # Our Deque is born here! It's empty when it starts, but don't worry, it will hold some amazing data soon.
+    #-----------------------------------------
     def __init__(self):
-        '''Initialize an empty Deque.'''
         self.items = []
 
-    # Let's add some functionality to our Deque. First up: adding items to the front!
-    def add_front(self, item):
-        '''Add an item to the front of the Deque.'''
-        self.items.append(item)
-
-    # Now, let's add an item to the back of the Deque.
-    def add_rear(self, item):
-        '''Add an item to the rear of the Deque.'''
-        self.items.insert(0, item)
-
-    # Making a Deque is no fun if we can't remove items as well. Let's remove from the front...
-    def remove_front(self):
-        '''Remove an item from the front of the Deque.'''
-        if self.isEmpty():
-            return None
-        else:
-            return self.items.pop()
-
-    # ...and also from the rear. Double the ends, double the fun!
-    def remove_rear(self):
-        '''Remove an item from the rear of the Deque.'''
-        if self.isEmpty():
-            return None
-        else:
-            return self.items.pop(0)
-
-    # Sometimes, it's useful to know if our Deque is lonely (empty). So let's add that functionality as well.
+    #-----------------------------------------
+    # Is our deque lonely? This snippet checks if there are any friends (data) in our deque
+    #-----------------------------------------
     def isEmpty(self):
-        '''Check if the Deque is empty.'''
         return self.items == []
 
-    # And of course, it's always good to know how big our Deque is. Let's make that possible.
+    #-----------------------------------------
+    # Let’s welcome a new friend (data) at the rear of our deque!
+    # And just like a cermony, the 'append' function is doing the welcoming part.
+    #-----------------------------------------
+    def addRear(self, item):
+        self.items.append(item)
+
+    #-----------------------------------------
+    # Well, deque also loves surprising people. So, here we are, welcoming a new guest (data) at the front of our deque!
+    #-----------------------------------------
+    def addFront(self, item):
+        self.items.insert(0, item)
+
+    #-----------------------------------------
+    # It’s time to say goodbye :( . This code snippet let’s a friend(data) leave from the front of our deque!
+    # See, deque is more flexible than we thought!
+    #-----------------------------------------
+    def removeFront(self):
+        return self.items.pop(0)
+
+    #-----------------------------------------
+    # Our deque is flexible both ways! It can also say goodbye to a friend(data) from the rear!
+    #-----------------------------------------
+    def removeRear(self):
+        return self.items.pop()
+
+    #-----------------------------------------
+    # Want to know how many friends (data) our deque has? This little code is our mini census for deque.
+    #-----------------------------------------
     def size(self):
-        '''Return the size of the Deque.'''
         return len(self.items)
 
-# That's it! We're done here. You now have a fully functional Deque at your disposal. 
-# Keep on coding and have a great day! Cheers!
-
-
+#-----------------------------------------
+# And voila! Our deque is ready! Skrrt Skrrt
+# Now you can go on an adventure of your own with this sweet deque! 
+# It will keep your data organized like a small but efficient library of Alexandria!
+# Keep Coding!
+#-----------------------------------------
