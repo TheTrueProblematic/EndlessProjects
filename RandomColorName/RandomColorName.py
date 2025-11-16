0a7b095f2276a92c39e8ce2b1d7262b74b304fe0
TheTrueProblematic <mwmttl@gmail.com>
2025-11-15 21:39:42 -0700

    Select random color names from a list.

---
+++ RandomColorName/RandomColorName.py
@@ -0,0 +1,25 @@
+
+# Howdy folks! This here is our fun little script, affectionately dubbed "RandomColorName".
+# What does it do? You might ask. Simple! It picks a random color name from a list.
+# Much like a colorful surprise, you never know what you're gonna get!
+
+# Importing our only friend in this lonesome plain, the 'random' module.
+# Don't worry, it's a standard Python module, so we aren't breaking any rules!
+import random
+
+# Here's our kaleidoscope of color names. Gander at it to your heart's content!
+color_names = ['Red', 'Blue', 'Green', 'Yellow', 'Pink', 'Purple', 'Orange', 'Black', 'White', 'Gray']
+
+# Now, onto the moment you've been waiting for. Drumroll please...
+
+# We use the 'choice' function from our friend Mr.Random to pick a color. 
+# Like the blindfolded kid at the piñata party, he has no bias!
+random_color = random.choice(color_names)
+
+# After it's been chosen, we display the chosen color. 
+# Excitement and anticipation much? Don't worry, thrill's not going away. Run the script again!
+print("The randomly selected color is:", random_color)
+
+# And that's all she wrote, folks! Pretty neat, huh? 
+# Keep on coding, y'all! Remember, every colourful line of code paints a brighter future!
+
